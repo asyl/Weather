ca7ad2fab132667f761bfeaa12c2257d7caf8b85
Asyl <asyl.dev@gmail.com>
2017-08-18 19:39:45 +0500

    initial commit 💡

---
+++ Weather/Utils/UIColor+Weather.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Weather.swift
+//  Weather
+//
+//  Created by Asyl Isakov on 2017/08/18.
+//  Copyright © 2017年 asyl. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  
+  class var wt_charcoal: UIColor { return 0x333333.color }
+
+}
---
+++ Weather/ViewControllers/WeatherList/WeatherListController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Asyl Isakov on 2017/08/18.
+//  Copyright © 2017年 asyl. All rights reserved.
+//
+
+import UIKit
+
+class WeatherListController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+
+
+}
+
