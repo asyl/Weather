9de9b527addd86d5f5598eef9dfe1b9b7c3acf4e
Asyl <asyl.dev@gmail.com>
2017-08-20 01:06:52 +0500

    cleaning Project 🚧

--- Weather/DarkSkyWeatherModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DarkSkyWeatherModel.swift
-//  Weather
-//
-//  Created by Asyl Isakov on 2017/08/19.
-//  Copyright © 2017年 asyl. All rights reserved.
-//
-
-import Foundation
--- Weather/Networking/ApiKey.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ApiKey.swift
-//  Weather
-//
-//  Created by Asyl Isakov on 2017/08/18.
-//  Copyright © 2017年 asyl. All rights reserved.
-//
-
-let darkSkyApiKey = "9492329ce55cbc27f7d7c50ecd0f027a"
